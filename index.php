e8b09f77ffc7fae9691108897ebbd1ca6ec5b739
Vladimir Dmitriev <defurator@mail.ru>
2015-04-10 12:57:58 +0300

    front begin

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Baget</title>
+  <link rel="stylesheet" type="text/css" href="css/all.css">
+  <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/all-old-ie.css">
+  <![endif]-->
+  <!--[if gt IE 8]>
+    <link rel="stylesheet" href="css/all.css">
+  <![endif]-->
+  <!--[if lt IE 10]> 
+  <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <div class="inner">
+    <header class="header">
+      <a href="/" class="header_logo">БелБагетМастер</a>
+      <div class="header_center">
+        <div class="header_slang"><p>Рамки на заказ в Минске</p></div>
+        <div class="hmenu">
+          <ul class="hmenu_list">
+            <li class="hmenu_item">
+              <a href="#" class="hmenu_link">Частным лицам</a>
+            </li>
+            <li class="hmenu_item">
+              <a href="#" class="hmenu_link">Юридическим лицам</a>
+            </li>
+            <li class="hmenu_item">
+              <a href="#" class="hmenu_link">Дизайнерам</a>
+            </li>
+            <li class="hmenu_item">
+              <a href="#" class="hmenu_link">Дилерам</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="header_info">
+        <div class="header_phone">
+          <p>8(029)175 10 48</p>
+          <p>8(029)224 56 05</p>
+        </div>
+        <div class="header_sub">
+          <p>Институт культуры</p>
+          <p>Пушкинская</p>
+          <p>Институт культуры</p>
+          <p>Пушкинская</p>
+        </div>
+        <div class="header_time">
+          <p>Звоните нам по будним</p>
+          <p>с 10.00 до 20.00</p>
+        </div>
+      </div>
+    </header>
+    <nav class="menu">
+      <ul class="menu_list">
+        <li class="menu_item">
+          <a href="#" class="menu_link">Главная</a>
+        </li>
+        <li class="menu_item">
+          <a href="#" class="menu_link">О компании</a>
+        </li>
+        <li class="menu_item">
+          <a href="#" class="menu_link">Услуги</a>
+        </li>
+        <li class="menu_item">
+          <a href="#" class="menu_link">Каталог багета</a>
+        </li>
+        <li class="menu_item">
+          <a href="#" class="menu_link">Галерея работ</a>
+        </li>
+        <li class="menu_item">
+          <a href="#" class="menu_link">Дисконтная программа</a>
+        </li>
+        <li class="menu_item">
+          <a href="#" class="menu_link">Контакты</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="slider">
+      
+    </div>
+    <div class="icon">
+      
+    </div>
+    <div class="quickmenu">
+      
+    </div>
+    <div class="counter">
+      
+    </div>
+    <footer class="footer">
+      
+    </footer>
+  </div>
+</body>
+</html>
