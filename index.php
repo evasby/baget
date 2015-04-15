0e8090e958dc6359e67af9201dfd01b53f2e1304
Vladimir Dmitriev <defurator@mail.ru>
2015-04-15 13:02:27 +0300

    foter and half of bx

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta charset="UTF-8">
   <title>Baget</title>
   <link rel="stylesheet" type="text/css" href="css/all.css">
   <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+  <script type="text/javascript" src="js/jquery.bxslider.js"></script>
   <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
@@ -17,6 +19,16 @@
   <!--[if lt IE 10]> 
   <script type="text/javascript" src="js/jquery.placeholder.js"></script>
   <![endif]-->
+  <script>
+    $(document).ready(function(){
+      $('.bxslider').bxSlider({
+        /*nextSelector: '#slider-next',
+        prevSelector: '#slider-prev',
+        nextText: 'Onward →',
+        prevText: '← Go back'*/
+      });
+    });
+  </script>
 </head>
 <body>
   <div class="inner">
@@ -81,29 +93,120 @@
         </li>
       </ul>
     </nav>
-    <div class="slider">
-      
-    </div>
-    <div class="icon">
-      
-    </div>
-    <div class="quickmenu">
-      
-    </div>
-    <div class="counter">
-      
+    <div class="content">
+      <div class="slider">
+        <ul class="bxslider">
+          <li><img src="/image/slider/slide1.jpg" /></li>
+          <li><img src="/image/slider/slide2.jpg" /></li>
+          <li><img src="/image/slider/slide3.jpg" /></li>
+          <li><img src="/image/slider/slide4.jpg" /></li>
+          <li><img src="/image/slider/slide5.jpg" /></li>
+        </ul>
+      </div>
+      <div class="icon">
+        
+      </div>
+      <div class="quickmenu">
+        
+      </div>
+      <div class="counter">
+        
+      </div>
     </div>
     <footer class="footer">
       <div class="follow">
         <div class="follow_title">Следуйте за нами</div>
         <div class="follow_list">
-          <div class="follow_item"><a href="#" class="follow_link follow_link_vk">Вконтакте</a></div>
-          <div class="follow_item"><a href="#" class="follow_link follow_link_ok">Одноклассники</a></div>
-          <div class="follow_item"><a href="#" class="follow_link follow_link_fb">Facebook</a></div>
-          <div class="follow_item"><a href="#" class="follow_link follow_link_ig">Instagram</a></div>
-          <div class="follow_item"><a href="#" class="follow_link follow_link_tt">Twitter</a></div>
-          <div class="follow_item"><a href="#" class="follow_link follow_link_yt">Наше видео</a></div>
+          <div class="follow_item"><a href="#" class="follow_link follow_link__vk">Вконтакте</a></div>
+          <div class="follow_item"><a href="#" class="follow_link follow_link__ok">Одноклассники</a></div>
+          <div class="follow_item"><a href="#" class="follow_link follow_link__fb">Facebook</a></div>
+          <div class="follow_item"><a href="#" class="follow_link follow_link__ig">Instagram</a></div>
+          <div class="follow_item"><a href="#" class="follow_link follow_link__tt">Twitter</a></div>
+          <div class="follow_item"><a href="#" class="follow_link follow_link__yt">Наше видео</a></div>
         </div>
+      </div>
+      <div class="footer_top">
+        <div class="fmenu">
+          <div class="fmenu_title">О нас</div>
+          <ul class="fmenu_list">
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Главная</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">О компании</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Услуги</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Каталог багета</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Галерея работ</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Дисконтная программа</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Наша команда</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Наша экспертность</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Отзывы</a>
+            </li>
+            <li class="fmenu_item">
+              <a href="" class="fmenu_link">Контакты</a>
+            </li>
+          </ul>
+        </div>
+        <div class="fmenu2">
+          <div class="fmenu2_title">Сотрудничество</div>
+          <div class="fmenu2_descr"><p>Мы предлагаем хорошие условия для сотрудничества, возможно с другими городами</p></div>
+          <ul class="fmenu2_list">
+            <li class="fmenu2_item">
+              <a href="" class="fmenu2_link">Дизайнерам</a>
+            </li>
+            <li class="fmenu2_item">
+              <a href="" class="fmenu2_link">Дилерам</a>
+            </li>
+            <li class="fmenu2_item">
+              <a href="" class="fmenu2_link">Частным лицам</a>
+            </li>
+            <li class="fmenu2_item">
+              <a href="" class="fmenu2_link">Юридическим лицам</a>
+            </li>
+          </ul>
+        </div>
+        <div class="fshops">
+          <div class="fshops_title">Магазины</div>
+          <div class="fshops_city">г.Минск</div>
+          <div class="fshops_item">
+            <p>Институт культуры,</p>
+            <p>ул.Могилевская 5 п.6</p>
+          </div>
+          <div class="fshops_item">
+            <p>Пушкинская,</p>
+            <p>ул.Дунина-Марцинкевича 11 ТЦ "Раковский Кирмаш" 2-й этаж пав.№17</p>
+          </div>
+        </div>
+        <div class="fcontacts">
+          <div class="fcontacts_title">Контакты</div>
+          <div class="fcontacts_phone">
+            <p>8(029)175 10 48</p>
+            <p>8(029)224 56 05</p>
+          </div>
+          <div class="fcontacts_email">
+            <a href="">buch@baget.by</a>
+          </div>
+          <div class="fcontacts_skype">
+            <p>Skype: bbm_office</p>
+          </div>
+        </div>
+      </div>
+      <div class="footer_bottom">
+        <a class="wf" href="https://webformat.by">Создание и продвижение сайтов</a>
       </div>
     </footer>
   </div>
---
+++ js/jQuery.GI.TheWall.js-master/README.md
@@ -0,0 +1,9 @@
+jQuery.GI.TheWall.js
+=====================
+
+This plugin could be used to create easily an image gallery similar to the google image search
+
+[Demo](http://goldinteractive.github.io/jQuery.GI.TheWall.js/)
+
+[![Build Status](https://travis-ci.org/Goldinteractive/jQuery.GI.TheWall.js.svg?branch=master)](https://travis-ci.org/Goldinteractive/jQuery.GI.TheWall.js)
+
